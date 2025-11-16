bded5c4f0dacec43f6d5d865f80b6a44cc115718
chanintorndti <chanintorn.dti@gmail.com>
2025-11-16 09:45:19 +0700

    complete V.1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <h1 className="text-center text-sm my-10">
+        Created by NinniN DTI SAU
+        <br />
+        Copyright © 2025 Southeast Asia University
+      </h1>
+    </div>
+  )
+}
